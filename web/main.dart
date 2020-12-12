4fd29655a16381b3982174770d356f72728c9bf9
Joel Leblanc <leblanc.js@icloud.com>
2020-12-11 20:19:30 -0500

    Added a task data object.

--- web/main.dart
+++ web/main.dart
@@ -1,31 +1,39 @@
 import 'dart:html';
 
+class Task {
+  final String id;
+  final String title;
+  final int points;
+
+  Task(this.id, this.title, this.points) {}
+}
+
 var tasks = [
-  {"id": "TEST-1", "title": "Do something cool", "points": 1},
-  {"id": "TEST-2", "title": "Do something even cooler", "points": 3},
-  {"id": "TEST-3", "title": "Hide the pain", "points": 5}
+  Task("TEST-1", "Do something cool", 1),
+  Task("TEST-2", "Do something even cooler", 3),
+  Task("TEST-3", "Hide the pain", 5)
 ];
 
 var points = [0, 0, 0, 0];
 
-DivElement createCard(String id, String name, int points) {
+DivElement createCard(Task task) {
   var div = Element.div();
   div.classes.add("card");
-  div.id = "card-${id}";
+  div.id = "card-${task.id}";
   div.attributes
-      .addAll({"draggable": "true", "data-points": points.toString()});
+      .addAll({"draggable": "true", "data-points": task.points.toString()});
 
   var elTitle = Element.div();
   elTitle.classes.add("card-title");
-  elTitle.text = id;
+  elTitle.text = task.id;
 
   var elName = Element.div();
   elName.classes.add("card-name");
-  elName.text = name;
+  elName.text = task.title;
 
   var elPoints = Element.div();
   elPoints.classes.add("card-points");
-  elPoints.text = "${points}";
+  elPoints.text = "${task.points}";
 
   div.children.addAll([elTitle, elName, elPoints]);
   return div;
@@ -67,10 +75,10 @@ void onDrop(MouseEvent event) {
 void main() {
   var pane1 = querySelector("#pane1").querySelector(".pane-flow");
 
-  for (var task in tasks) {
-    pane1.children.add(createCard(task["id"], task["title"], task["points"]));
-    points[0] += task["points"];
-  }
+  tasks.forEach((task) {
+    pane1.children.add(createCard(task));
+    points[0] += task.points;
+  });
 
   pane1.parent.querySelector(".pane-points").text = "${points[0]} points";
 
